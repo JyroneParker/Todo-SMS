75a69534f311c56177924a9c9265575a9c751606
jyrone.parker@gmail.com <vagrant@homestead>
2015-10-19 01:27:05 +0000

    Added controller logic for Facebook flow.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -62,4 +62,26 @@ class AuthController extends Controller
             'password' => bcrypt($data['password']),
         ]);
     }
+
+    /**
+    * Redirect the user to the Facebook authentication page.
+    *
+    * @return Response
+    */
+   public function redirectToProvider()
+   {
+       return Socialite::driver('facebook')->redirect();
+   }
+
+   /**
+    * Obtain the user information from Facebook.
+    *
+    * @return Response
+    */
+   public function handleProviderCallback()
+   {
+       $user = Socialite::driver('facebook')->user();
+
+       // $user->token;
+   }
 }
