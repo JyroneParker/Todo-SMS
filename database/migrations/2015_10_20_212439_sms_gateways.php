88741321cdb4caec5bb3fd4dddd2dd131f97f8fa
Jyrone Parker <jyrone.parker@gmail.com>
2015-10-20 14:27:18 -0700

    Added SMS Gateway

---
+++ database/migrations/2015_10_20_212439_sms_gateways.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SmsGateways extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gateways', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('gateway');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('gateways');
+    }
+}
