cc0b5d8c96b69f619ac14acffc48bf9185c8c5c4
jyrone.parker@gmail.com <vagrant@homestead>
2015-10-21 17:30:40 +0000

    Prettied it up

--- resources/assets/views/welcome.blade.php
+++ resources/assets/views/welcome.blade.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
     <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Todo SMS</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
@@ -26,31 +25,65 @@
         <script>
         (adsbygoogle = window.adsbygoogle || []).push({});
         </script>
+        <style>
+            html, body {
+                height: 100%;
+            }
 
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+            .facebook{
+              border-radius: 50px;
+            }
+            #facebook{
+              width:350px;
+              height:105px;
+            }
+        </style>
 
     </head>
     <body>
 
         <div class="container">
             <div class="content">
-              <h1 class="text-center">Todo SMS</h1>
-              <p class="text-center"> This simple app allows you to create a todo item and send it to yourself as a text message. If you want persistence log in with Facebook!</p>
+              <h1>Todo SMS</h1>
+              <p> This simple app allows you to create a todo item and send it to yourself as a text message. If you want persistence log in with Facebook!</p>
               @if(isset($user))
-              <h2 class="text-center">Welcome back {{$user['name']}}</h2>
+              <h2>Welcome back {{$user['name']}}</h2>
               <img class="facebook" src="{{$user['avatar']}}"/>
               @foreach ($todos as $todo)
-              <p class="text-center">{{ $todo->content }}</p>
+              <p>{{ $todo->content }}</p>
               @endforeach
-              <form method="POST" action="/addTodo" role="form">
+              <form method="POST" action="/addTodo">
                 {!! csrf_field() !!}
 
-                <div class="form-group">
+                <div>
 
-                    <input class="form-control" type="text" name="content" placeholder="Content" value="{{ old('content') }}">
+                    <input type="text" name="content" placeholder="Content" value="{{ old('content') }}">
                     <br>
-                    <input class="form-control" type="tel" name="phone" placeholder="10 digit phone number" value="{{ old('phone') }}">
-                    <br>
-                    <select class="form-control" name="gateway">
+                    <input type="tel" name="phone" placeholder="10 digit phone number" value="{{ old('phone') }}">
+                    <select name="gateway">
                       @foreach($gateways as $gateway)
                       <option value="{{$gateway->gateway}}">{{ $gateway->name }}</option>
                       @endforeach
@@ -59,21 +92,20 @@
 
 
 
-                <div class="form-group">
-                    <button class="btn btn-success" type="submit">Send Todo </button>
+                <div>
+                    <button type="submit">Add Todo </button>
                 </div>
               </form>
               @else
-              <form method="POST" action="/addTodo" role="form">
+              <form method="POST" action="/addTodo">
                 {!! csrf_field() !!}
 
-                <div class="form-group text-center">
+                <div>
 
-                    <input class="form-control" type="text" name="content" placeholder="Content" value="{{ old('content') }}">
+                    <input type="text" name="content" placeholder="Content" value="{{ old('content') }}">
                     <br>
-                    <input class="form-control" type="tel" name="phone" placeholder="10 digit phone number" value="{{ old('phone') }}">
-                    <br>
-                    <select class="form-control" name="gateway">
+                    <input type="tel" name="phone" placeholder="10 digit phone number" value="{{ old('phone') }}">
+                    <select name="gateway">
                       @foreach($gateways as $gateway)
                       <option value="{{$gateway->gateway}}">{{$gateway->name}}</option>
                       @endforeach
@@ -82,10 +114,9 @@
 
 
 
-                <div class="form-group">
-                    <button class="btn btn-success" type="submit">Send Todo </button>
-                    <a class="btn btn-default" href="/auth/facebook"><span class="glyphicon glyphicon-user"></span> Connect To Facebook</a>
-
+                <div>
+                    <button type="submit">Add Todo </button> <br> <a href="/auth/facebook">
+                      <img id="facebook" src="http://cdn.wind8apps.com/wp-content/uploads/2015/06/facebook-connect.png"/></a>
                 </div>
               </form>
               @endif
