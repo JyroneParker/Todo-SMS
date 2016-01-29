095d2c284c96aa92b55d6513e3e1b3fd1ac42faf
Jyrone Parker <jyrone.parker@gmail.com>
2016-01-29 02:29:57 +0000

    ads

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,6 +70,18 @@ The best part? It's 100% free!" />
               @foreach ($todos as $todo)
               <p class="text-center">{{ $todo->content }}</p>
               @endforeach
+              <center>
+              <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+              <!-- Todo Top Banner -->
+              <ins class="adsbygoogle"
+                   style="display:block"
+                   data-ad-client="ca-pub-7023023584987784"
+                   data-ad-slot="3867992558"
+                   data-ad-format="auto"></ins>
+              <script>
+              (adsbygoogle = window.adsbygoogle || []).push({});
+              </script>
+            </center>
               <form method="POST" action="/addTodo" role="form">
                 {!! csrf_field() !!}
 
